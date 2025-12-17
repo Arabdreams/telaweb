abc92015672ab4bbbdf7a989728c71f1103671fd
Arabdreams <ameerbssplanworld@gmail.com>
2025-12-17 12:18:18 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-    images: {
-        domains: ['tela-assets.s3.me-south-1.amazonaws.com'],
-      },
-    
-    // distDir: "dist",
-    // output: "export",
-    compiler: {
-        styledComponents: {
-            displayName: true,
-            ssr: true,
-            fileName: true,
-            cssProp: true,
-            minify: true,
-        },
+  output: "export", // <-- enables full static export
+  images: {
+    domains: ["tela-assets.s3.me-south-1.amazonaws.com"],
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      cssProp: true,
+      minify: true,
     },
+  },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
