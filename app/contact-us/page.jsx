49475406abebf240d7dbdfd9d8449a6d1f319be0
Arabdreams <ameerbssplanworld@gmail.com>
2025-12-17 15:08:57 +0300

    Update page.jsx

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,11 +1,5 @@
 'use client';
-
 export const dynamic = 'force-dynamic';
-export const dynamicParams = true;
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
-export const runtime = 'nodejs';
-export const preferredRegion = 'auto';
 
 import ContactUs from "@/screens/ContactUs";
 
