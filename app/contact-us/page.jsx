f21f9329e08bbfa8ad7d60b1dcc976d39bcfd965
Arabdreams <ameerbssplanworld@gmail.com>
2025-12-17 11:50:59 +0300

    Update page.jsx

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,7 +1,10 @@
-import ContactUs from "@/screens/ContactUs";
+// /app/contact-us/page.jsx
+import dynamic from "next/dynamic";
+
+const ContactForm = dynamic(() => import("@/screens/ContactUs"), { ssr: false });
 
 const page = () => {
-    return <ContactUs />;
+  return <ContactForm />;
 };
 
 export default page;
