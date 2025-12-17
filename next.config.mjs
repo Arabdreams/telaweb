6d82586e4e8ea3817170ee909af17ea167a5c5bf
Arabdreams <ameerbssplanworld@gmail.com>
2025-12-17 15:35:06 +0300

    Add image domain and compiler options to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    domains: ['tela-assets.s3.me-south-1.amazonaws.com'],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
+    },
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      cssProp: true,
+      minify: true,
     },
   },
 };
