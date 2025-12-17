bd83300a4de66fe9794f4de79fa4c156eee7c3fc
Arabdreams <ameerbssplanworld@gmail.com>
2025-12-17 15:00:48 +0300

    Refactor next.config.mjs for standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-    images: {
-        domains: ['tela-assets.s3.me-south-1.amazonaws.com'],
-      },
-    
-    // distDir: "dist",
-    // output: "export",
-    compiler: {
-        styledComponents: {
-            displayName: true,
-            ssr: true,
-            fileName: true,
-            cssProp: true,
-            minify: true,
-        },
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
     },
+  },
 };
 
 export default nextConfig;
